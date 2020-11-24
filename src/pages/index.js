7b38d06c697abc39ef7b0998a5009d8b70a6d008
Shar <shar@servicebot.io>
2020-11-23 22:22:03 -0500

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,7 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-      Test Commit i did but doesnt let me commit to main im going to try forking
-      this one more time. umm maybe a github permission error, i can commit to
-      the main repo probably because it's in my github account
+      Toooo
     </div>
     <Link to="/demo-1/">Go to Demo 1</Link> <br />
     <Link to="/demo-2/">Go to Demo 2 - with billing page css</Link> <br />
