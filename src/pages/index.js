2e323aabde18fb9103675ad5db7decb6322b3cd2
Shar <shar@servicebot.io>
2020-11-24 15:53:14 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,48 +8,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div class="h-screen bg-white overflow-hidden flex">
       <Sidebar />
-      <div className="flex flex-col w-0 flex-1 overflow-hidden">
-        <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
-          <button className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-            <span className="sr-only">Open sidebar</span>
-            {/* Heroicon name: menu */}
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
-        <main
-          className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
-          tabIndex={0}
-        >
-          <div className="py-6">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              <h1 className="text-2xl font-semibold text-gray-900">
-                Dashboard dfgdfg
-              </h1>
-            </div>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              {/* Replace with your content */}
-              <div className="py-4">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-              </div>
-              {/* /End replace */}
-            </div>
-          </div>
-        </main>
-      </div>
     </div>
     {/* Static sidebar for desktop */}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
