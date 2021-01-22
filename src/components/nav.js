1c38c80ffbe39f0841aaba38a13443baa126abfb
darafsheh <shar@servicebot.io>
2021-01-21 21:39:26 -0800

    Added Peloton pricing page

--- src/components/nav.js
+++ src/components/nav.js
@@ -106,6 +106,14 @@ const Nav = ({}) => {
                       </span>
                       </Link>
                     </div>
+                    <div className={`${pricingPages ? '' : 'hidden'} space-y-1`}>
+                      <Link to="/pricing-pages/peloton-pricing" activeClassName="bg-gray-100 text-gray-900" className="group w-full flex items-center pl-11 pr-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
+                      <span class="w-2.5 h-2.5 mr-4 bg-green-500 rounded-full" aria-hidden="true"></span>
+                      <span class="truncate">
+                          Peloton Pricing
+                      </span>
+                      </Link>
+                    </div>
                 </div>
                 <div className="space-y-1">
                     <button onClick={() => setCustomerPortalsOn(!customerPortals)} className="group w-full flex items-center pl-2 pr-1 py-2 text-sm font-medium rounded-md bg-white text-gray-600 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500">
