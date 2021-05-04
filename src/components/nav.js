b532c8f2b444c5d5c72f5f1b0c877563bbd06fb3
darafsheh <shar@servicebot.io>
2021-05-04 13:05:29 -0700

    Flat + Metered pricing

--- src/components/nav.js
+++ src/components/nav.js
@@ -127,6 +127,14 @@ const Nav = ({}) => {
                       </Link>
                     </div>
                     <div className={`${pricingPages ? '' : 'hidden'} space-y-1`}>
+                      <Link to="/pricing-pages/flat-plus-metered" activeClassName="bg-gray-100 text-gray-900" className="group w-full flex items-center pl-11 pr-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
+                      <span class="w-2.5 h-2.5 mr-4 bg-green-500 rounded-full" aria-hidden="true"></span>
+                      <span class="truncate">
+                          Flat + Metered Pricing
+                      </span>
+                      </Link>
+                    </div>
+                    <div className={`${pricingPages ? '' : 'hidden'} space-y-1`}>
                       <Link to="/pricing-pages/peloton-pricing" activeClassName="bg-gray-100 text-gray-900" className="group w-full flex items-center pl-11 pr-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
                       <span class="w-2.5 h-2.5 mr-4 bg-green-500 rounded-full" aria-hidden="true"></span>
                       <span class="truncate">
